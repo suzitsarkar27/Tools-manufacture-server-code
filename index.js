41e09f9521fd36e34cfa4c52127fb8e8023bc030
suzitsarkar27 <96899998+suzitsarkar27@users.noreply.github.com>
2022-05-27 03:40:41 +0600

    delete-api

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qcofh.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ebrgm.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
@@ -35,12 +35,28 @@ async function run() {
       res.send( services);
     });
 
+
+    app.get('/order', async (req, res) => {
+      const query = {};
+      const cursor = oderCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+
     app.post("/order", async (req, res) => {
       const newData = req.body;
       const result = await oderCollection.insertOne(newData);
       res.send(result);
     });
 
+
+    app.delete("/order/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await oderCollection.deleteOne(query);
+      res.send(result);
+    });
   }
   finally {
 
