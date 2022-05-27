7ecbf667533196d6f4fe208cace6e901df2b4fe3
suzitsarkar27 <96899998+suzitsarkar27@users.noreply.github.com>
2022-05-27 13:40:01 +0600

    delet-order

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ebrgm.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.noqb8.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 
 
 async function run() {
