e3014bc4b7dab73a1fdd1df44cf7357720c8cf70
suzitsarkar27 <96899998+suzitsarkar27@users.noreply.github.com>
2022-05-28 01:46:48 +0600

    add-userdata

--- index.js
+++ index.js
@@ -9,9 +9,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.noqb8.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vylix.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
 
 
 async function run() {
@@ -19,7 +18,7 @@ async function run() {
     await client.connect();
     const serviceCollection = client.db('toolsManufacturer').collection('data');
     const oderCollection = client.db('toolsManufacturer').collection('order');
-
+    const userCollection = client.db('toolsManufacturer').collection('users');
 
     app.get('/data', async (req, res) => {
       const query = {};
@@ -64,6 +63,18 @@ async function run() {
       const result = await oderCollection.deleteOne(query);
       res.send(result);
     });
+
+    app.put('/user/:email',async(req,res)=>{
+      const email =req.params.email;
+      const user=req.body;
+      const filter={email:email};
+      const options ={upsert:true};
+      const updateDoc={
+        $set:user,
+      }
+         const result =await userCollection.updateOne(filter,updateDoc,options);
+         res.send(result)
+    })
   }
   finally {
 
