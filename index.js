ab888e499f9903dbcb2dde26d4fc5c9fc4f54b82
suzitsarkar27 <96899998+suzitsarkar27@users.noreply.github.com>
2022-08-12 20:50:26 +0600

    update-data

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,16 +12,16 @@ app.use(cors());
 app.use(express.json());
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5a7ib.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ayppg3j.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
 async function run() {
   try {
     await client.connect();
-    const servicecollection = client.db("user").collection("service");
-    const ordercollection = client.db("user").collection("orders");
-    const usercollection = client.db("user").collection("users");
+    const servicecollection = client.db("Products").collection("data");
+    // const ordercollection = client.db("user").collection("orders");
+    // const usercollection = client.db("user").collection("users");
 
 
     app.get('/data', async (req, res) => {
@@ -46,64 +46,65 @@ async function run() {
     });
 
 
-    // app.get('/order', async (req, res) => {
-    //   const query = {};
-    //   const cursor =ordercollection.find(query);
-    //   const result = await cursor.toArray();
-    //   res.send(result);
-    // });
+    app.get('/order', async (req, res) => {
+      const query = {};
+      const cursor =ordercollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-    // app.get('/order', async (req, res) => {
-    //   const email =req.query.email;
-    //   const query={email:email}
-    //   const result =await usercollection.find(query).toArray();
+    app.get('/order', async (req, res) => {
+      const email =req.query.email;
+      const query={email:email}
+      const result =await usercollection.find(query).toArray();
 
-    //   res.send(result);
-    // });
-
-
-    // app.post("/order", async (req, res) => {
-    //   const newData = req.body;
-    //   const result = await ordercollection.insertOne(newData);
-    //   res.send(result);
-    // });
+      res.send(result);
+    });
 
 
-    // app.delete("/order/:id", async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: ObjectId(id) };
-    //   const result = await ordercollection.deleteOne(query);
-    //   res.send(result);
-    // });
+    app.post("/order", async (req, res) => {
+      const newData = req.body;
+      const result = await ordercollection.insertOne(newData);
+      res.send(result);
+    });
 
-    //  app.get('/user',async(req,res) =>{
-    //    const users = await usercollection.find().toArray();
-    //    res.send(users)
-    //  })
+
+    app.delete("/order/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await ordercollection.deleteOne(query);
+      res.send(result);
+    });
+
+     app.get('/user',async(req,res) =>{
+       const users = await usercollection.find().toArray();
+       res.send(users)
+     })
      
-    //  app.put('/user/admin/:email',async(req,res)=>{
-    //     const email =req.params.email;
-    //     const filter ={ email:email}
-    //     const updateDoc ={
-    //       set: {role:'admin'},
-    //     };
-    //     const result =await usercollection.updateOne(filter,updateDoc);
-    //     res.send(result)
-    //  })
+     app.put('/user/admin/:email',async(req,res)=>{
+        const email =req.params.email;
+        const filter ={ email:email}
+        const updateDoc ={
+          set: {role:'admin'},
+        };
+        const result =await usercollection.updateOne(filter,updateDoc);
+        res.send(result)
+     })
 
 
-    // app.put('/user/:email',async(req,res)=>{
-    //   const email =req.params.email;
-    //   const user=req.body;
-    //   const filter={email:email};
-    //   const options ={upsert:true};
-    //   const updateDoc={
-    //     $set:user,
-    //   }
-    //   const result =await usercollection.updateOne(filter,updateDoc,options)
-    //   // const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
-    //   // res.send({ result, token });
-    //   res.send(result)
+    app.put('/user/:email',async(req,res)=>{
+      const email =req.params.email;
+      const user=req.body;
+      const filter={email:email};
+      const options ={upsert:true};
+      const updateDoc={
+        $set:user,
+      }
+      const result =await usercollection.updateOne(filter,updateDoc,options)
+      // const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+      // res.send({ result, token });
+      res.send(result)
+
     })
   }
 
